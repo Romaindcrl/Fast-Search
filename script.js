e550a1ca21acc0b2b24e30a40d7b805b8ab650a3
TheFlute07 <81828658+TheFlute07@users.noreply.github.com>
2021-04-06 13:16:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ function strUcFirst(a){return (a+'').charAt(0).toUpperCase()+a.substr(1);}
 
 function clearSearch() {
     if(document.getElementById("searchForm").value == ""){
-        document.getElementById("nom").innerHTML = "Chien";
-        document.getElementByClassName("definition0").innerHTML = "Mammifère domestique dont de nombreuses races sont élevées ; spécialement le mâle (opposé à chienne).";
-        document.getElementByClassName("definition1").innerHTML = "Avec hostilité.";
-        document.getElementByClassName("definition2").innerHTML = "Rencontrer bien des difficultés.";
+        var search ="color";
+        loadSearch(search);
 
     }
 }
 
-function loadSearch() {
-  var search = document.getElementById("searchForm").value;
+function loadSearch(search) {
+  if(search == "search"){
+      var search = document.getElementById("searchForm").value;
+  }
   var url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + search;
   xhttp.open("GET", url, true);
   xhttp.send(); 
@@ -33,10 +33,10 @@ xhttp.onreadystatechange = function() {
     if(xhttp.readyState == 4 && xhttp.status == 200) {
         document.getElementById("definition").innerHTML = " ";
         var response = JSON.parse(xhttp.responseText);
-        document.getElementById("nom").innerHTML = strUcFirst(response["0"].word);
-        i = 0
-        while(i != 3) {
-            document.getElementById("definition").innerHTML += "<div id='definitions' class='definition"+ length +"'>"+strUcFirst(response['0']['meanings']['0']['definitions'][i].definition)+"</div>"
+        document.getElementById("name").innerHTML = strUcFirst(response["0"].word);
+        i = 0;
+        while(i != response[0].meanings[0].definitions.length) {
+            document.getElementById("definition").innerHTML += "<div id='definitions' class='definition"+ i +"'>"+strUcFirst(response['0']['meanings']['0']['definitions'][i].definition)+"</div>"
             i += 1;
         }
     }
